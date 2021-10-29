201003f0bf77e547c9b8067d4338a433c463b972
Dicky Erfan Septiono <62490370+dicky495@users.noreply.github.com>
2021-10-29 10:58:02 +0700

    Add files via upload

---
+++ todolist/header.php
@@ -0,0 +1,8 @@
+<div class="container-fluid wrap bg-warning" style="padding: 2%;">
+    <div class="row">
+        <div align="center">
+            <h1><b>TODO LIST PDAM</b></h1>
+            <p>Daftar Pekerjaan </p>
+        </div>
+    </div>
+</div>
---
+++ todolist/login.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Login</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm bg-dark text-light rounded">
+                    <form>
+                        <div class="text-center">
+                            <h1>Login</h1>
+                            <p>Silakan login untuk membuat todo</p>
+                        </div>
+                        <div class="mb-3">
+                            <label for="email" class="form-label">Email address</label>
+                            <input type="email" class="form-control" id="email">
+                        </div>
+                        <div class="mb-3">
+                            <label for="password" class="form-label">Password</label>
+                            <input type="password" class="form-control" id="password" name="password">
+                        </div>
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </form>
+                    <div align="center">
+                        <p>belum punya akun ? </p>
+                        <a href="register.php"><button class="btn btn-danger">Register</button></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="js/bootstrap.bundle.js" crossorigin="anonymous"></script>
+
+</body>
+
+</html>
